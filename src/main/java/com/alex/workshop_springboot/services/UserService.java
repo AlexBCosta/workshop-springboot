21ff47e3b17a08dde46ed7e58007f1d989a9c617
Alex Costa <89145796+AlexBCosta@users.noreply.github.com>
2024-10-21 20:59:00 -0300

    Atualização User

--- src/main/java/com/alex/workshop_springboot/services/UserService.java
+++ src/main/java/com/alex/workshop_springboot/services/UserService.java
@@ -27,7 +27,21 @@ public class UserService {
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	 public void delete(Long id) {
-		 userRepository.deleteById(id);
-	 }
+
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());
+		entity.setTelefone(obj.getTelefone());
+
+	}
 }
